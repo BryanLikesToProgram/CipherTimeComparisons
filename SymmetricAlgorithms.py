9e7eb884501ed553cf0b927c89b0f05aa549ff88
BryanLikesToProgram <bryan.racic@angrysquirrels.com>
2019-02-07 01:51:45 -0500

    Added Fernet Support
    -Still needs architecture, efficiency, and readability updates

--- SymmetricAlgorithms.py
+++ SymmetricAlgorithms.py
@@ -4,19 +4,21 @@ import io
 import cryptography
 from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
 from cryptography.hazmat.backends import default_backend
+from cryptography.fernet import Fernet
 
 class AES:
     def __init__(self, filename) :
         self.fileName = filename
-        print("Cipher: AES");
-    def counterMode(self) :
+        print("Cipher: AES")
+
+    def counterMode(self, keySize, nonceSize) :
         print("Mode: Counter")
         inFile = open(self.fileName, 'rb')
         fileData = inFile.read()
         inFile.close()
 
-        key = os.urandom(32) # 128 bit key (average key length)
-        init_val = os.urandom(16)
+        key = os.urandom(keySize) # average key length = 128 Bits
+        init_val = os.urandom(nonceSize)
 
         cipher = Cipher(algorithms.AES(key), modes.CTR(init_val), backend=default_backend())  # Counter mode converts blocks to streams, no padding 
         encryptor = cipher.encryptor()
@@ -40,3 +42,39 @@ class AES:
         outFile = open("decrypted.mp4", "wb")
         outFile.write(encryptedData)
         outFile.close()
+        print("")
+
+class FERNET:
+
+    def __init__(self, filename) :
+        self.fileName = filename
+        print("Cipher: Fernet")
+
+    def singleFernet(self) :
+        print("Mode: Single")
+        inFile = open(self.fileName, 'rb')
+        fileData = inFile.read()
+        inFile.close()
+
+        key = Fernet.generate_key()
+        cipher = Fernet(key)
+        startEncTime = time.time()
+        #----------- Start
+        encryptedData = cipher.encrypt(fileData)
+        #----------- End
+        endEncTime = time.time() - startEncTime
+        print("Encrypt Time: " + str(endEncTime))
+
+        startDecTime = time.time()
+        #----------- Start 
+        encryptedData = cipher.decrypt(encryptedData)
+        #----------- End
+        endDecTime = time.time() - startDecTime
+        print("Decrypt Time: " + str(endEncTime))
+        outFile = open("decrypted.mp4", "wb")
+        outFile.write(encryptedData)
+        outFile.close()
+        print("")
+
+    def multiFernet(self) :
+        print( "Not yet implimented" )
--- ciphertest.py
+++ ciphertest.py
@@ -1,4 +1,5 @@
 from SymmetricAlgorithms import AES
+from SymmetricAlgorithms import FERNET
 
-AES("test.mp4").counterMode()
-
+AES("test.mp4").counterMode(32, 16)
+FERNET("test.mp4").singleFernet()
