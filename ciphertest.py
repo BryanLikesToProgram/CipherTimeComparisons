225fe00e48443f926672f0f612a82d11cdc835ac
Bryan Racic <bryan.racic@angrysquirrels.com>
2019-02-21 05:18:55 -0500

    Update ciphertest.py

--- ciphertest.py
+++ ciphertest.py
@@ -263,15 +263,3 @@ if __name__ == "__main__":
     except ArgsError:
         print('Unexpected ArgumentError: ')
         raise
-
-from SymmetricAlgorithms import AES
-from SymmetricAlgorithms import FERNET
-from SymmetricAlgorithms import XOR
-from ASymmetricAlgorithms import ELLIPTICCURVE
-from ASymmetricAlgorithms import RSA
-
-AES("test.mp4").counterMode(32, 16)
-FERNET("test.mp4").singleFernet()
-ELLIPTICCURVE("test.mp4").ThreeEightFourROne()
-#RSA("test.mp4").PemSerialized() Not applicable for larger files
-XOR("test.mp4").simpleXor()
